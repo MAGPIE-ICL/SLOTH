d14a8f261aef9c739dffa087fd77f9984d4403bc
Stefano Merlini <65250880+stemerlini@users.noreply.github.com>
2026-03-04 20:55:54 +0000

    Fix y-direction ray projection ordering to match legacy solver

--- src/shared/propagation.py
+++ src/shared/propagation.py
@@ -51,26 +51,19 @@ def ray_to_Jonesvector(rays, *, ne_extent = None, probing_direction = 'z', keep_
         ray_p = ray_p.at[3].set(jnp.arctan(vz / vx))
     # XZ plane
     elif(probing_direction == 'y'):
-        #
-        # I have switched x & z for the sake of consistent ordering of the axes
-        # Standardised in keeping with positive 'forward' notation, etc. x * y = z but don't do y * x = -z
-        # If memory is not a concern then will instead create a class to cover directions
-        # This would entail both the array and a self.dir parameter of type char - containing 'x', 'y' or 'z'
-        #
-
         # Positions on plane
         if not keep_current_plane:
             t_bp = (y - ne_extent) / vy
 
-            ray_p = ray_p.at[0].set(z - vz * t_bp)
-            ray_p = ray_p.at[2].set(x - vx * t_bp)
+            ray_p = ray_p.at[0].set(x - vx * t_bp)
+            ray_p = ray_p.at[2].set(z - vz * t_bp)
         else:
-            ray_p = ray_p.at[0].set(z)
-            ray_p = ray_p.at[2].set(x)
+            ray_p = ray_p.at[0].set(x)
+            ray_p = ray_p.at[2].set(z)
 
         # Angles to plane
-        ray_p = ray_p.at[1].set(jnp.arctan(vz / vy))
-        ray_p = ray_p.at[3].set(jnp.arctan(vx / vy))
+        ray_p = ray_p.at[1].set(jnp.arctan(vx / vy))
+        ray_p = ray_p.at[3].set(jnp.arctan(vz / vy))
     # XY plane
     elif(probing_direction == 'z'):
         # Positions on plane
@@ -117,17 +110,10 @@ def back_propogate(rays, ne_extent, probing_direction):
     elif(probing_direction == 'y'):
         t_bp = (y - ne_extent) / vy
 
-        #
-        # I have switched x & z for the sake of consistent ordering of the axes
-        # Standardised in keeping with positive 'forward' notation, etc. x * y = z but don't do y * x = -z
-        # If memory is not a concern then will instead create a class to cover directions
-        # This would entail both the array and a self.dir parameter of type char - containing 'x', 'y' or 'z'
-        #
-
         # Positions on plane
-        rays = rays.at[0].set(z - vz * t_bp)
+        rays = rays.at[0].set(x - vx * t_bp)
         rays = rays.at[1].set(ne_extent)
-        rays = rays.at[2].set(x - vx * t_bp)
+        rays = rays.at[2].set(z - vz * t_bp)
     # XY plane
     elif(probing_direction == 'z'):
         t_bp = (z - ne_extent) / vz
